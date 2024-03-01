947fb6b6d3f78727b48b3f270f59200722af193d
!!Dean <dwhall256@gmail.com>
2024-02-29 21:48:50 -0600

    Replace printf with puts in panic handler

--- src/panicoverride.nim
+++ src/panicoverride.nim
@@ -1,20 +1,21 @@
 # Simplest:
+#[
 proc rawoutput(s: string) = discard
 proc panic(s: string) = discard
+]#
 
 # A more elaborate option
-#[
-proc printf(frmt: cstring) {.varargs, importc, header: "<stdio.h>", cdecl.}
+proc puts(s: cstring) {.importc, header: "<stdio.h>", cdecl.}
+proc putchar(c: int) {.importc, header: "<stdio.h>", cdecl.}
 proc exit(code: int) {.importc, header: "<stdlib.h>", cdecl.}
+let EXIT_FAILURE {.importc, header: "<stdlib.h>", nodecl.}: int
 
 {.push stack_trace: off, profiler:off.}
 
-proc rawoutput(s: string) =
-  printf("%s\n", s)
-
-proc panic(s: string) =
-  rawoutput(s)
-  exit(1)
+proc panic*(s: string) =
+  puts(s.cstring)
+  const newline = ord('\n')
+  putchar(newline)
+  exit(EXIT_FAILURE)
 
 {.pop.}
-]#
